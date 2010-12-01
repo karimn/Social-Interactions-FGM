86669ff39a5a37b92e56b360bd9a883ec7b0bd3c
Karim Nagib <karimn@bu.edu>
2010-12-01 15:35:35 -0500

    Adding initialization function

--- FgmData.R
+++ FgmData.R
@@ -5,6 +5,121 @@ library(maptools)
 setClass("FgmData",
          representation(cluster.info = "data.frame"),
          contains = "SpatialPointsDataFrame")
+         
+FgmData.cols = quote(c(v000, v001, v002, v003, v004, v005, v023, v024, v025, v104,
+                                               v130, v151, v190, g116, v106, v155, v467b, v467c, v467d, v467e,
+                                               v511, v704, v705, v714, v716, v717, v719, v721, v729, v730,
+                                               g102, g106, g107, g118, g119, sgovern, s103g, s912, s915,
+                                               v394,
+                                               s917a:s917x,
+                                               v3a08j, v3a08q))
+#                                               m2a, m2b, m2g, m2k, m2n,
+#                                               m3a, m3b, m3g, m3k, m3n,
+#                                               m14, m15, m43, m44, m57a, m57b, m57e, m57f, m57g,
+#                                               m57i, m57j, m57k, m57l, m57m, m57n, m57o, m57p, m57q, m57r,
+#                                               m57x,
+#                                               m70, m72, m73,
+#sdcol.1:sdcol.7, s906.1:s906.7, s908.1:s908.7, s909.1:s909.7, s910.1:s910.7, s911.1:s911.7))
+
+                                                           # To consider: "reason did not deliver at health facility"
+FgmData.circum.info <- c('circum.info.tv', 'circum.info.radio', 'circum.info.papers', 'circum.info.pamph', 'circum.info.poster', 'circum.info.comm', 'circum.info.hw.home', 'circum.info.hw.facil', 'circum.info.husb', 'circum.info.relfr', 'circum.info.other')
+
+FgmData.col.names <- c(
+               'phase', 'cluster', 'hh', 'respond', 'area.unit', 'wt', 'domain', 'region', 'urban.rural', 'years.lived.res', 
+               'religion', 'hh.head.sex', 'wealth.index', 'intends.circum', 'educ.lvl', 'literacy', 'med.help.permission', 'med.help.money', 'med.help.distance', 'med.help.transportation',
+               'marital.age', 'partner.occupation.1', 'partner.occupation.2', 'working', 'occupation.1', 'occupation.2', 'work.for.whom', 'work.home', 'partner.educlvl', 'partner.age',
+               'mother.circum', 'mother.circum.age', 'mother.circum.bywhom', 'circum.byreligion', 'circum.continue', 'governorate', 'prev.governate', 'num.not.circum', 'discuss.circum',
+               'visit.health.facil.12mon',
+               FgmData.circum.info,
+               'nofpuse.husbandopposed', 'nofpuse.noaccess.toofar' 
+#               'prenatal.doctor', 'prenatal.nurse', 'prenatal.daya', 'prenatal.other', 'prenatal.noone',
+#               'assist.doctor', 'assist.nurse', 'assist.daya', 'assist.other', 'assist.noone', 
+#               'antenatal.visits', 'place.delivery', 'preg.told.complicate', 'preg.told.where.complicate', 'antenatal.care.home', 'antenatal.care.otherhome', 'antenatal.care.govthosp.urban', 'antenatal.care.govthu.urban', 'antenatal.care.govthealthoff', 
+#               'antenatal.care.govthosp.rural', 'antenatal.care.govthu.rural', 'antenatal.care.mch', 'antenatal.care.govt.other', 'antenatal.care.privhosp', 'antenatal.care.privdoc', 'antenatal.care.fpassoc', 'antenatal.care.csi', 'antenatal.care.ngo', 'antenatal.care.priv.other', 
+#               'antenatal.care.other',
+#               'postnatal.check.2mon', 'postnatal.check.who', 'postnatal.check.where'
+               )
+
+FgmData.occupation.2.levels <- c(0:9) #, 98, 99)
+FgmData.occupation.2.labels <- c('no work', 'prof., tech., manag.', 'clerical', 'sales', 'agri-self employed', 'agri-employee', 'hh & domestic', 
+                         'services', 'skilled manual', 'unskilled manual') #, "don't know", 'missing')
+
+FgmData.med.help.levels <- 1:2 #,9
+FgmData.med.help.labels <- c('big problem', 'not big problem') #, 'missing')
+FgmData.religions <- c("muslim", "christian")
+FgmData.literacy.labels <- c('cannot read', 'reads with difficulty', 'reads easily')
+FgmData.partner.educlvl.labels <- c('no educ', 'incomplete primary', 'complete primary', 'incomplete secondary', 'complete secondary', 'higher')
+
+setMethod("initialize", "FgmData",
+          function(.Object, ir.file, gps.file, dhs.year = 2008, ...)
+          {
+            ir <- read.dta(ir.file, convert.underscore = TRUE)
+            fgm.data <- subset(ir, select = eval(FgmData.cols))
+            
+            rm(ir)
+
+            num.col <- length(FgmData.col.names)
+            names(fgm.data)[1:num.col] <- FgmData.col.names
+
+            fgm.data <- within(fgm.data, 
+            {
+              dhs.year <- dhs.year
+              hh.id <- factor(paste(cluster, hh, sep = '-'))
+              religion <- factor(religion, 
+                                 levels = 1:2, 
+                                 labels = FgmData.religions) #, "missing"))
+              literacy.fac <- factor(literacy, 
+                                     levels = c(0:2), #, 9), 
+                                     labels = FgmData.literacy.labels) #, 'missing'))
+              med.help.permission.fac <- factor(med.help.permission, 
+                                                levels = FgmData.med.help.levels,
+                                                labels = FgmData.med.help.labels)
+              med.help.distance.fac <- factor(med.help.distance, 
+                                              levels = FgmData.med.help.levels, 
+                                              labels = FgmData.med.help.labels)
+              med.help.transportation.fac <- factor(med.help.transportation, 
+                                                    levels = FgmData.med.help.levels,
+                                                    labels = FgmData.med.help.labels)
+              med.help.money.fac <- factor(med.help.money, 
+                                                    levels = FgmData.med.help.levels,
+                                                    labels = FgmData.med.help.labels)
+              partner.occupation.1.fac <- factor(partner.occupation.1)
+              partner.occupation.2.fac <- factor(partner.occupation.2, levels = FgmData.occupation.2.levels, labels = FgmData.occupation.2.labels)
+              occupation.1.fac <- factor(occupation.1)
+              occupation.2.fac <- factor(occupation.2, levels = FgmData.occupation.2.levels, labels = FgmData.occupation.2.labels)
+              partner.educlvl.fac <- factor(partner.educlvl, 
+                                            levels = c(0:5), #, 8, 9), 
+                                            labels = FgmData.partner.educlvl.labels) #, "don't know", 'missing'))
+              mother.circum.fac <- factor(mother.circum, 
+                                          levels = c(0:1), #, 9), 
+                                          labels = c('no', 'yes')) #, 'missing'))
+
+              wealth.index.2 <- factor(ifelse(wealth.index %in% c("rich", "richest"), 1, 0), levels = c(0, 1), labels = c("poor", "rich"))
+
+              discuss.circum.fac <- factor(discuss.circum, level = 0:1, labels = c("no", "yes"))
+              visit.health.facil.12mon.fac <- factor(visit.health.facil.12mon, level = 0:1, labels = c("no", "yes"))
+            })
+
+            for (ci in FgmData.circum.info)
+              fgm.data[,paste(ci, "fac", sep = ".")] <- factor(fgm.data[,ci], levels = 0:1, labels = c("no", "yes"))
+
+            fgm.data <- subset(fgm.data, select = c(cluster:hh.id)) 
+            fgm.data <- fgm.data[ order(fgm.data$hh.id, fgm.data$birth.year), ]
+
+            if (is.null(fgm.data$unique.cluster)) 
+              fgm.data$unique.cluster <- as.numeric(row.names(fgm.data))
+
+            gps <- read.dbf(gps.file)
+
+            fgm.spdf <- merge(fgm.data, gps, by.x = c('dhs.year', 'cluster'), by.y = c('DHSYEAR', 'DHSCLUST'))
+            coordinates(fgm.spdf) <- c("LONGNUM", "LATNUM")
+            proj4string(fgm.spdf) <- CRS("+proj=longlat +ellps=WGS84")
+
+            rm(gps)
+
+            callNextMethod(.Object, fgm.spdf, cluster.info = cluster.info, ...)
+          }
+)
 
 if (isGeneric("cluster.coordinates<-")) removeGeneric("cluster.coordinates<-")
 setGeneric("cluster.coordinates<-", function(object, value) standardGeneric("cluster.coordinates<-"))
