04c4ee8493b401b60bcffbb92a72ac55ada969a3
Karim Naguib <karimn2.0@gmail.com>
2012-03-09 19:43:40 -0500

    Some table row names

--- table.global.R
+++ table.global.R
@@ -7,17 +7,32 @@ birth.order.fac.info <- list(label = "Birth Order", dict = birth.order.regs)
 cohort.range <- 1989:1996
 youngest.cohort <- max(cohort.range) 
 
-# exog.se.regs <- list(
-#     spat.grpavg.wealth.index.2_rich.10 = "
-#     spat.grpavg.urban.rural_urban.10
-#     spat.grpavg.educ.lvl_primary.10
-#     spat.grpavg.educ.lvl_secondary.10
-#     spat.grpavg.educ.lvl_higher.10 
-#     spat.grpavg.marital.age.10
-#     spat.grpavg.mother.circum.fac_yes.10
-#     spat.grpavg.religion_christian.10
-#     spat.grpavg.hh.head.sex_female.10
-#     spat.grpavg.med.help.distance.fac_big_problem.10
-#     spat.grpavg.discuss.circum.fac_yes.10
-#     spat.grpavg.received.info.circum.10
+exog.se.regs <- list(
+ spat.grpavg.wealth.index.2_rich.10 = "Wealth (Rich)",
+ spat.grpavg.urban.rural_urban.10 = "Residence (Urban)",
+ spat.grpavg.educ.lvl_primary.10 = "Education (Primary)",
+ spat.grpavg.educ.lvl_secondary.10 = "Education (Secondary)",
+ spat.grpavg.educ.lvl_higher.10 = "Education (Higher)",
+ spat.grpavg.marital.age.10 = "Marital Age",
+ spat.grpavg.mother.circum.fac_yes.10 = "Mother FGM",
+ spat.grpavg.religion_christian.10 = "Religion (Christian)",
+ spat.grpavg.hh.head.sex_female.10 = "HH Head Sex (Female)",
+ spat.grpavg.med.help.distance.fac_big_problem.10 = "Medical Help Dist. (Big Problem)",
+ spat.grpavg.discuss.circum.fac_yes.10 = "Discussed FGM",
+ spat.grpavg.received.info.circum.10 = "Received Info on FGM"
+)
 
+exog.se.intran.regs <- list(
+ spat.intran.grpavg.wealth.index.2_rich.10 = "Wealth (Rich)",
+ spat.intran.grpavg.urban.rural_urban.10 = "Residence (Urban)",
+ spat.intran.grpavg.educ.lvl_primary.10 = "Education (Primary)",
+ spat.intran.grpavg.educ.lvl_secondary.10 = "Education (Secondary)",
+ spat.intran.grpavg.educ.lvl_higher.10 = "Education (Higher)",
+ spat.intran.grpavg.marital.age.10 = "Marital Age",
+ spat.intran.grpavg.mother.circum.fac_yes.10 = "Mother FGM",
+ spat.intran.grpavg.religion_christian.10 = "Religion (Christian)",
+ spat.intran.grpavg.hh.head.sex_female.10 = "HH Head Sex (Female)",
+ spat.intran.grpavg.med.help.distance.fac_big_problem.10 = "Medical Help Dist. (Big Problem)",
+ spat.intran.grpavg.discuss.circum.fac_yes.10 = "Discussed FGM",
+ spat.intran.grpavg.received.info.circum.10 = "Received Info on FGM"
+)
